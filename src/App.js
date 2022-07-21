40d9a7ba29f19752bbb50b98fa4b173bad26a2bb
Dmitry <d.a16@yandex.ru>
2022-07-21 15:42:27 +0300

    creating cards with props

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Footer from "./components/Footer";
 
 import './App.scss';
 
+const arr = [
+   { name: 'Solimo Coffee Beans 2 kg', alt: 'Solimo Coffee Beans', price: '10.73', imageUrl: '/images/coffee/1.jpg' },
+   { name: 'Presto Coffee Beans 1 kg', alt: 'Presto Coffee Beans', price: '15.99', imageUrl: '/images/coffee/2.jpg' },
+   { name: 'AROMISTICO Coffee 1 kg', alt: 'AROMISTICO Coffee', price: '6.93', imageUrl: '/images/coffee/3.jpg' },
+   { name: 'Lavazza Caffe Espresso 2 kg', alt: 'Lavazza Caffe Espresso', price: '13.79', imageUrl: '/images/coffee/4.jpg' },
+   { name: 'Bourbon Espresso Manchester 320 g', alt: 'Bourbon Espresso Manchester', price: '5.11', imageUrl: '/images/coffee/5.jpg' },
+   { name: 'Kopi Luwak Roasied Coffee 370 g', alt: 'Kopi Luwak Roasied Coffee', price: '12.65', imageUrl: '/images/coffee/6.jpg' },
+   { name: 'Jamaica Blue Mountain Coffee 2 kg', alt: 'Jamaica Blue Mountain Coffee', price: '15.99', imageUrl: '/images/coffee/7.jpg' },
+   { name: '100% Arabica Cafedo Brasil 320 g', alt: '100% Arabica Cafedo Brasil', price: '9.79', imageUrl: '/images/coffee/8.jpg' }
+]
+
 function App() {
    return (
       <>
@@ -22,119 +33,9 @@ function App() {
                   </div>
                </div>
                <div className="cards">
-                  <Card />
-                  <div className="card">
-                     <img className="favorite" src="/images/heart-unliked.svg" alt="unliked" />
-                     <img className="img" src="/images/coffee/2.jpg" alt="Presto Coffee Beans" />
-                     <h3>Presto Coffee Beans 1 kg</h3>
-                     <div className="card__bottom">
-                        <div className="card__bottom-price">
-                           <p>Price:</p>
-                           <b>15.99 $</b>
-                        </div>
-                        <button>
-                           <svg width="24" height="24" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#CD853F"/>
-                           </svg>
-                        </button>
-                     </div>
-                  </div>
-                  <div className="card">
-                     <img className="favorite" src="/images/heart-unliked.svg" alt="unliked" />
-                     <img className="img" src="/images/coffee/3.jpg" alt="" />
-                     <h3>AROMISTICO Coffee 1 kg</h3>
-                     <div className="card__bottom">
-                        <div className="card__bottom-price">
-                           <p>Price:</p>
-                           <b>6.93 $</b>
-                        </div>
-                        <button>
-                           <svg width="24" height="24" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#CD853F"/>
-                           </svg>
-                        </button>
-                     </div>
-                  </div>
-                  <div className="card">
-                     <img className="favorite" src="/images/heart-unliked.svg" alt="unliked" />
-                     <img className="img" src="/images/coffee/4.jpg" alt="Lavazza Caffe Espresso" />
-                     <h3>Lavazza Caffe Espresso 2 kg</h3>
-                     <div className="card__bottom">
-                        <div className="card__bottom-price">
-                           <p>Price:</p>
-                           <b>13.79 $</b>
-                        </div>
-                        <button>
-                           <svg width="24" height="24" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#CD853F"/>
-                           </svg>
-                        </button>
-                     </div>
-                  </div>
-                  <div className="card">
-                     <img className="favorite" src="/images/heart-unliked.svg" alt="unliked" />
-                     <img className="img" src="/images/coffee/5.jpg" alt="Bourbon Espresso Manchester" />
-                     <h3>Bourbon Espresso Manchester 320 g</h3>
-                     <div className="card__bottom">
-                        <div className="card__bottom-price">
-                           <p>Price:</p>
-                           <b>5.11 $</b>
-                        </div>
-                        <button>
-                           <svg width="24" height="24" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#CD853F"/>
-                           </svg>
-                        </button>
-                     </div>
-                  </div>
-                  <div className="card">
-                     <img className="favorite" src="/images/heart-unliked.svg" alt="unliked" />
-                     <img className="img" src="/images/coffee/6.jpg" alt="Kopi Luwak Roasied Coffee" />
-                     <h3>Kopi Luwak Roasied Coffee 370 g</h3>
-                     <div className="card__bottom">
-                        <div className="card__bottom-price">
-                           <p>Price:</p>
-                           <b>12.65 $</b>
-                        </div>
-                        <button>
-                           <svg width="24" height="24" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#CD853F"/>
-                           </svg>
-                        </button>
-                     </div>
-                  </div>
-                  <div className="card">
-                     <img className="favorite" src="/images/heart-unliked.svg" alt="unliked" />
-                     <img className="img" src="/images/coffee/7.jpg" alt="Jamaica Blue Mountain Coffee" />
-                     <h3>Jamaica Blue Mountain Coffee 2 kg</h3>
-                     <div className="card__bottom">
-                        <div className="card__bottom-price">
-                           <p>Price:</p>
-                           <b>15.99 $</b>
-                        </div>
-                        <button>
-                           <svg width="24" height="24" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#CD853F"/>
-                           </svg>
-                        </button>
-                     </div>
-                  </div>
-                  <div className="card">
-                     <img className="favorite" src="/images/heart-unliked.svg" alt="unliked" />
-                     <img className="img" src="/images/coffee/8.jpg" alt="100% Arabica Cafedo Brasil" />
-                     <h3>100% Arabica Cafedo Brasil 320 g</h3>
-                     <div className="card__bottom">
-                        <div className="card__bottom-price">
-                           <p>Price:</p>
-                           <b>9.79 $</b>
-                        </div>
-                        <button>
-                           <svg width="24" height="24" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#CD853F"/>
-                           </svg>
-                        </button>
-                     </div>
-                  </div>
+                  {arr.map((obj) => (
+                     <Card name={obj.name} alt={obj.alt} price={obj.price} imageUrl={obj.imageUrl} />
+                  ))}
                </div>
             </div>
          </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,20 @@
-function Card() {
+function Card(props) {
+   
+   const onClickButton = () => {
+      alert(props.price);
+   }
+
    return (
       <div className="card">
-         <img className="favorite" src="/images/heart-unliked.svg" alt="unliked" />
-         <img className="img" src="/images/coffee/1.jpg" alt="Solimo Coffee Beans" />
-         <h3>Solimo Coffee Beans 2 kg</h3>
+         <button className="card__favorite"><span>&#9825;</span></button>
+         <img className="img" src={props.imageUrl} alt={props.alt} />
+         <h3>{props.name}</h3>
          <div className="card__bottom">
             <div className="card__bottom-price">
                <p>Price:</p>
-               <b>10.73 $</b>
+               <b>{props.price} $</b>
             </div>
-            <button>
-               <svg width="24" height="24" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#CD853F"/>
-               </svg>
-            </button>
+            <button className="card__add" onClick={props.onClick} ><span>&#43;</span></button>
          </div>
       </div>
    )
